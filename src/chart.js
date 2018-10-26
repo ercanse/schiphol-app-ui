3ece911c0be0294f37dc00c25690763c25fdce7d
Selman Ercan <selman.ercan@kpn.com>
2018-10-26 21:47:29 +0200

    Reformatting

--- src/chart.js
+++ src/chart.js
@@ -1,9 +1,8 @@
 import React, {Component} from 'react';
-import { scaleBand, scaleLinear } from 'd3-scale';
-import { tsvParse } from 'd3-dsv';
-import { max } from 'd3-array';
-import { axisBottom, axisLeft } from 'd3-axis';
-import { select } from 'd3-selection';
+import {scaleBand, scaleLinear} from 'd3-scale';
+import {max} from 'd3-array';
+import {axisBottom, axisLeft} from 'd3-axis';
+import {select} from 'd3-selection';
 import axios from 'axios';
 
 import './chart.css';
@@ -26,14 +25,14 @@ class BarChart extends Component {
 
         const svgWidth = 960, svgHeight = 500;
 
-        const margin = { top: 20, right: 20, bottom: 30, left: 40 },
-          width = svgWidth - margin.left - margin.right,
-          height = svgHeight - margin.top - margin.bottom;
+        const margin = {top: 20, right: 20, bottom: 30, left: 40},
+            width = svgWidth - margin.left - margin.right,
+            height = svgHeight - margin.top - margin.bottom;
 
         const x = scaleBand()
-            .rangeRound([0, width])
-            .padding(0.1),
-          y = scaleLinear().rangeRound([height, 0]);
+                .rangeRound([0, width])
+                .padding(0.1),
+            y = scaleLinear().rangeRound([height, 0]);
 
         var data = this.state.data[0];
         var keys = [];
@@ -48,31 +47,31 @@ class BarChart extends Component {
         y.domain([0, max(values)]);
 
         return <svg width={svgWidth} height={svgHeight}>
-           <g transform={`translate(${margin.left}, ${margin.top})`}>
-             <g
-               className="axis axis--x"
-               transform={`translate(0, ${height})`}
-               ref={node => select(node).call(axisBottom(x))}
-             />
-             <g className="axis axis--y">
-               <g ref={node => select(node).call(axisLeft(y).ticks(10))} />
-               {}
-               <text transform="rotate(-90)" y="6" dy="0.71em" textAnchor="end">
-                 Frequency
-               </text>
-             </g>
-             {data.map(d => (
-               <rect
-                 key={d.pier}
-                 className="bar"
-                 x={x(d.pier)}
-                 y={y(d.frequency)}
-                 width={x.bandwidth()}
-                 height={height - y(d.frequency)}
-               />
-             ))}
-           </g>
-         </svg>;
+            <g transform={`translate(${margin.left}, ${margin.top})`}>
+                <g
+                    className="axis axis--x"
+                    transform={`translate(0, ${height})`}
+                    ref={node => select(node).call(axisBottom(x))}
+                />
+                <g className="axis axis--y">
+                    <g ref={node => select(node).call(axisLeft(y).ticks(10))}/>
+                    {}
+                    <text transform="rotate(-90)" y="6" dy="0.71em" textAnchor="end">
+                        Frequency
+                    </text>
+                </g>
+                {data.map(d => (
+                    <rect
+                        key={d.pier}
+                        className="bar"
+                        x={x(d.pier)}
+                        y={y(d.frequency)}
+                        width={x.bandwidth()}
+                        height={height - y(d.frequency)}
+                    />
+                ))}
+            </g>
+        </svg>;
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import Chart from './chart';
 import * as serviceWorker from './serviceWorker';
 
