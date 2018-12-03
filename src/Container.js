2e602149162d22c6e746836999799a0049b50255
Selman Ercan <selman.ercan@kpn.com>
2018-12-03 13:23:59 +0100

    Variables instead of literals

--- src/Container.js
+++ src/Container.js
@@ -2,15 +2,18 @@ import React, {Component} from 'react';
 import Chart from './chart';
 import CirclePack from './circlePack';
 
+const CHART = 'CHART';
+const CIRCLEPACK = 'CIRCLEPACK';
+
 class Container extends Component {
     constructor(props) {
         super(props);
-        this.state = {active: "CIRCLEPACK"};
+        this.state = {active: CIRCLEPACK};
     }
 
     handleClick() {
         var active = this.state.active;
-        var newActive = active === 'CHART' ? 'CIRCLEPACK' : 'CHART';
+        var newActive = active === CHART ? CIRCLEPACK : CHART;
         this.setState({
             active: newActive
         });
@@ -18,14 +21,15 @@ class Container extends Component {
 
     render() {
         var active = this.state.active;
+        var buttonText = active === CHART ? 'Switch to circle pack' : 'Switch to bar chart';
         return (
             <div>
                 <button type="button" onClick={this.handleClick.bind(this)}>
-                    Toggle
+                    {buttonText}
                 </button>
-                {active === 'CHART' ? (
+                {active === CHART ? (
                     <Chart/>
-                ) : active === 'CIRCLEPACK' ? (
+                ) : active === CIRCLEPACK ? (
                     <CirclePack/>
                 ) : null}
             </div>
